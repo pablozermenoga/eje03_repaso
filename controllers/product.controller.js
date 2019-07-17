065a23de2fc49238af01aed9d5eb3b8ac3be9a25
pablozermeno <45905503+pablozermeno@users.noreply.github.com>
2019-07-17 10:37:39 -0600

    COMMIT UPDATE Y FINDID

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -35,6 +35,22 @@ const findAll=(req,res)=>{
         });
 }
 
+const findUneP = (req, res) => {
+    const {id}=req.params;
+    const params = {
+        _id:id
+    };
+    _product.findOne(params)
+        .then((data) =>{
+            res.status(status.OK);
+            res.json({msg:"Exito!!!",data:data});
+        })
+        .catch((err) =>{
+            res.status(status.NOT_FOUND);
+            res.json({msg:"Error!!! No se encontro",err:err})
+        });
+}
+
 const deleteByI=(req,res)=>{
     const {id}=req.params;
     _product.findByIdAndRemove({_id:id})
@@ -50,11 +66,30 @@ const deleteByI=(req,res)=>{
 }
 
 
-module.exports = (Product) => {
-    _product = Product;
+const updateByIdP = (req,res) =>{
+    const {id} = req.params;
+    const product = req.body;
+
+    const params = {
+        _id:id
+    }
+    _product.findByIdAndUpdate(params,product)
+        .then((data)=>{
+            res.status(status.OK);
+            res.json({msg:"Update correcto",data:data});
+        })
+        .catch((err)=>{
+            res.status(status.NOT_FOUND);
+            res.json({msg:"Error",err:err});
+        })
+}
+module.exports = (product) => {
+    _product = product;
     return({
         createProduct,
         findAll,
-        deleteByI
+        deleteByI,
+        updateByIdP,
+        findUneP
     });
 }
--- routers/product.router.js
+++ routers/product.router.js
@@ -7,12 +7,19 @@ module.exports = (wagner) => {
 
     router.get('/',(req,res)=>
         productCtrl.findAll(req,res));
+    
+    router.get('/:id',(req,res)=>
+        productCtrl.findUneP(req,res));
 
     router.post('/', (req, res) =>
         productCtrl.createProduct(req, res));
 
     router.delete('/:id',(req,res)=>
         productCtrl.deleteByI(req,res));
+    
+    router.put('/:id',(req,res)=>
+        productCtrl.updateByIdP(req,res));
+    
     
     return router
 }
